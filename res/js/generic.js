a7f4ca621070f9e57678a76ed299ef9fc0b3be4a
EngineerCoding <EngineerCoding@hotmail.nl>
2020-02-23 16:17:53 +0100

    Start on blog page

--- res/js/generic.js
+++ res/js/generic.js
@@ -2,6 +2,6 @@
 	// Enable the .enable-js components
 	var displayEnableJsComponents = document.querySelectorAll(".enable-js");
 	for (var i = 0; i < displayEnableJsComponents.length; i++) {
-		displayEnableJsComponents[i].style.display = "block";
+		displayEnableJsComponents[i].classList.remove("enable-js");
 	}
 })();
