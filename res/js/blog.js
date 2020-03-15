5fce3548653ee750320c05f37d1111a19334b8ab
EngineerCoding <EngineerCoding@hotmail.nl>
2020-03-15 17:08:28 +0100

    Prepare rendering full blog posts

--- res/js/blog.js
+++ res/js/blog.js
@@ -1,18 +1,67 @@
 (function() {
-	function initBlogPost(blogPost) {
+	var overviewContainer = document.getElementById("overview-container");
+	var loadingContainer = document.getElementById("loading-container");
 
-	}
+	var baseUrl = "/blog.html";
 
-	var main = document.getElementsByTagName("main")[0];
-
-	function renderBlogPost(blogPost) {
-		var postEntry = loadTemplate("card", {
+	function initBlogPost(blogPost, node) {
+		var node = loadTemplate("card", {
 			"card-image-source": blogPost.banner,
 			"card-metadata": (new Date()).toLocaleString(),
 			"card-title": blogPost.title
+		}).children[0];
+
+		var slug = blogPost.title.toLowerCase().replace(/\s/g, "-");
+		node.addEventListener("click", function(event) {
+			window.history.pushState(null, "", baseUrl + "#" + slug);
+			renderPage();
 		});
-		initBlogPost(blogPost);
-		main.appendChild(postEntry);
+
+		overviewContainer.appendChild(node);
+	}
+
+	function renderGenericError() {
+		var loadingSpinner = loadingContainer.querySelector(".loading");
+		loadingContainer.removeChild(loadingSpinner);
+		var error = document.createElement("h1");
+		error.classList.add("default-font");
+		error.style.color = "red";
+		error.innerText = "Something went wrong!";
+		loadingContainer.appendChild(error);
+	}
+
+	function renderFullBlogPost(data) {
+		console.log("Rendering full blog post");
+	}
+
+	function renderPage() {
+		if (window.location.hash.length === 0) {
+			loadingContainer.style.display = "none";
+			overviewContainer.style.display = "flex";
+		} else {
+			overviewContainer.style.display = "none";
+			loadingContainer.style.display = "flex";
+			var slug = window.location.hash.substring(1);
+			var data = window.sessionStorage.getItem(slug);
+			if (data != null) {
+				renderFullBlogPost(JSON.parse(data));
+			} else {
+				fetch("/res/data/blogs/" + slug + ".json")
+					.then(function(response) {
+						if (!response.ok) {
+							window.location.href = "/404.html";
+							return new Promise(function() { });
+						}
+						return response.json();
+					})
+					.then(function(data) {
+						renderFullBlogPost(data);
+						window.sessionStorage.setItem(slug, data);
+						loadingContainer.style.display = "none";
+					})
+					.catch(renderGenericError);
+			}
+		}
 	}
 
 	fetch("/res/data/blog_posts.json")
@@ -24,9 +73,10 @@
 				return b.published - a.published;
 			});
 
-			main.innerHTML = "";
-			main.classList.remove("centered-component");
-			main.classList.add("items-component");
-			blogPosts.forEach(renderBlogPost);
-		});
+			blogPosts.forEach(initBlogPost);
+			renderPage();
+		})
+		.catch(renderGenericError);
+
+	window.addEventListener("popstate", renderPage);
 })();
