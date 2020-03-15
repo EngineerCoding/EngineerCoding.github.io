31ecc8f87431b3763b5778e2f149adaabd2967de
EngineerCoding <EngineerCoding@hotmail.nl>
2020-03-15 16:00:27 +0100

    Add loading animation

--- res/js/blog.js
+++ res/js/blog.js
@@ -3,6 +3,8 @@
 
 	}
 
+	var main = document.getElementsByTagName("main")[0];
+
 	function renderBlogPost(blogPost) {
 		var postEntry = loadTemplate("card", {
 			"card-image-source": blogPost.banner,
@@ -10,7 +12,7 @@
 			"card-title": blogPost.title
 		});
 		initBlogPost(blogPost);
-		document.getElementsByTagName("main")[0].appendChild(postEntry);
+		main.appendChild(postEntry);
 	}
 
 	fetch("/res/data/blog_posts.json")
@@ -19,8 +21,12 @@
 		})
 		.then(function(blogPosts) {
 			blogPosts.sort(function(a, b) {
-				return (a.published - b.published) * -1;
+				return b.published - a.published;
 			});
+
+			main.innerHTML = "";
+			main.classList.remove("centered-component");
+			main.classList.add("items-component");
 			blogPosts.forEach(renderBlogPost);
 		});
 })();
